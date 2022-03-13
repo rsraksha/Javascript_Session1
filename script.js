91b55e9baa7576a4f67aab52e9b24b6dd7771553
R S Raksha <rsrakshars1@gmail.com>
2022-03-13 13:02:49 +0530

    add

---
+++ script.js
@@ -0,0 +1,69 @@
+var string="Skill Safari is an Elearning platform focusing on providing Skill Enhancement Training modules."
+console.log("String = " + string)
+
+var num = 34
+console.log("Integer value = " + num)
+
+var float = 77.9
+console.log("Float value = " + float)
+
+var boolean = false 
+console.log("Boolean value = " + boolean)
+
+// Area of circle - Using VAR
+console.log("Using VAR - ")
+var radius=34.5
+var PI = 3.142
+var area = PI * radius*radius
+console.log("Area of circle = "+ area)
+
+//Circumference of circle - Using VAR
+
+var radius=4.5
+var circum = 2 * PI * radius
+console.log("Circumference of circle = "+ circum)
+
+//Radius of circle - Using VAR
+
+var circum=26.798
+var radius = circum/2 * PI
+console.log("Radius of circle = "+ radius)
+
+
+// Area of circle - Using LET
+console.log("Using LET - ")
+let r = 3.5
+let pi = 3.142
+let area_ = pi * r*r
+console.log("Area of circle = "+ area_)
+
+//Circumference of circle - Using LET
+
+let circum_ = 2 * pi * r
+console.log("Circumference of circle = "+ circum_)
+
+//Radius of circle  - Using LET
+
+let circumference =6.798
+let rad = circumference/2 * pi
+console.log("Radius of circle = "+ rad)
+
+
+// Area of circle - Using CONST
+console.log("Using CONST - ")
+var radius=4.785
+var PI = 3.142
+const AREA= PI * radius*radius
+console.log("Area of circle = "+ AREA)
+
+//Circumference of circle - Using CONST
+
+var radius=1.5
+const CIRCUM = 2 * PI * radius
+console.log("Circumference of circle = "+ CIRCUM)
+
+//Radius of circle  - Using CONST
+
+var circum=28
+const RADIUS = circum/2 * PI
+console.log("Radius of circle = "+ RADIUS)
